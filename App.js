2bdef2564a854bb4aba93d42261918d2340fb627
Aya Debbagh <adebb027@uottawa.ca>
2024-03-20 20:21:16 -0400

    first changes

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Animation } from 'react-native';
+import React, {useRef, useEffect} from 'react';
+import { useFonts } from "expo-font";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    "BrightCircle": require("./assets/fonts/Bright-Circle-Font-by-Keithzo-BF65df84b7c2f07.otf"),
+  });
+  if (!fontsLoaded) {
+    return <Text>Loading...</Text>;
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <Text style={styles.logo}>Readius.</Text>
+
       <StatusBar style="auto" />
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#2D2429",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  default: {
+    fontSize: 20,
+  },
+  logo: {
+    flex: 1,
+    fontFamily: "BrightCircle", 
+    color: "#ECEFE8", 
+    fontSize: 75,
+
+  }
+ 
 });
