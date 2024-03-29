87f33839264664e77a462f03a91391fb8967a641
Aya Debbagh <adebb027@uottawa.ca>
2024-03-29 12:33:45 -0400

    Added navigation

--- App.js
+++ App.js
@@ -1,92 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Animation, TextInput, Platform, Button } from 'react-native';
-import React, {useRef, useEffect} from 'react';
-import { TouchableOpacity} from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Animation,
+  TextInput,
+  Platform,
+  Button,
+} from "react-native";
+import React, { useRef, useEffect } from "react";
+import { TouchableOpacity } from "react-native";
 import { useFonts } from "expo-font";
-import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-import { collection, getDocs } from "firebase/firestore"; 
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from "./screens/HomeScreen";
 
+const Stack = createStackNavigator();
 
 export default function App() {
   const [fontsLoaded] = useFonts({
-    "BrightCircle": require("./assets/fonts/Bright-Circle-Font-by-Keithzo-BF65df84b7c2f07.otf"),
-    "GartSerif": require("./assets/fonts/gartseriftrial-medium.otf"),
+    BrightCircle: require("./assets/fonts/Bright-Circle-Font-by-Keithzo-BF65df84b7c2f07.otf"),
+    GartSerif: require("./assets/fonts/gartseriftrial-medium.otf"),
   });
   if (!fontsLoaded) {
     return <Text>Loading...</Text>;
   }
   return (
-    <KeyboardAwareScrollView resetScrollToCoords={{ x: 0, y: 0 }} contentContainerStyle={styles.container} scrollEnabled={false}>
-
-      <Text style={styles.logo}>Readius.</Text>
-      <View style={styles.roundedRectangle}>
-        <TextInput style={styles.emailInput} keyboardType="email-address" placeholder="Email" />
-        <TextInput style={styles.emailInput} secureTextEntry={true} placeholder="Password" />
-
-      </View>
-      <TouchableOpacity style={styles.signUpButton} onPress={() => { /* handle sign up here */ }}>
-        <Text style={styles.buttonText}>Sign in</Text>
-      </TouchableOpacity>
-      <Text style={styles.createAccount}>Don't have an account? Create one</Text>
-
-      <StatusBar style="auto" />
-      </KeyboardAwareScrollView>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="HomeScreen">
+        <Stack.Screen
+          name="HomeScreen"
+          component={HomeScreen}
+          options={{
+            title: "Home",
+            headerShown: false,
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#2D2429",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-
-  logo: {
-    fontFamily: "BrightCircle", 
-    color: "#ECEFE8", 
-    fontSize: 75,
-    marginBottom: 50,
-
-  },
-  roundedRectangle: {
-    backgroundColor: "#ECEFE8",
-    height: 187,
-    width: 330,
-    borderRadius: 32,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  emailInput: {
-    marginTop: 15,
-    fontFamily: "GartSerif",
-    borderColor: "#C8C2D3",
-    borderWidth: 4,
-    borderRadius: 29,
-    padding: 10,
-    width: 267,
-    height: 46, 
-  },
-  signUpButton: {
-    elevation: 8,
-    backgroundColor: "#C8C2D3",
-    borderRadius: 32,
-    paddingVertical: 11,
-    paddingHorizontal: 70,
-    marginTop: 30,
-  },
-  buttonText: {
-    fontFamily: "GartSerif",
-    fontSize: 24,
-    color: "2D2429",
-  },
-  createAccount: {
-    fontFamily: "GartSerif",
-    fontSize: 16,
-    color: "#ECEFE8",
-    marginTop: 15,
-
-  }
- 
-});
---
+++ screens/HomeScreen.js
@@ -0,0 +1,109 @@
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Animation,
+  TextInput,
+  Platform,
+  Button,
+} from "react-native";
+import React, { useRef, useEffect } from "react";
+import { TouchableOpacity } from "react-native";
+import { useFonts } from "expo-font";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { collection, getDocs } from "firebase/firestore";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
+
+export default function App() {
+  return (
+    <KeyboardAwareScrollView
+      resetScrollToCoords={{ x: 0, y: 0 }}
+      contentContainerStyle={styles.container}
+      scrollEnabled={false}
+    >
+      <Text style={styles.logo}>Readius.</Text>
+      <View style={styles.roundedRectangle}>
+        <TextInput
+          style={styles.emailInput}
+          keyboardType="email-address"
+          placeholder="Email"
+        />
+        <TextInput
+          style={styles.emailInput}
+          secureTextEntry={true}
+          placeholder="Password"
+        />
+      </View>
+      <TouchableOpacity
+        style={styles.signUpButton}
+        onPress={() => {
+          /* handle sign up here */
+        }}
+      >
+        <Text style={styles.buttonText}>Sign in</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => {}}>
+        <Text style={styles.createAccount}>
+          Don't have an account? Create one
+        </Text>
+      </TouchableOpacity>
+
+      <StatusBar style="auto" />
+    </KeyboardAwareScrollView>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#2D2429",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  logo: {
+    fontFamily: "BrightCircle",
+    color: "#ECEFE8",
+    fontSize: 75,
+    marginBottom: 50,
+  },
+  roundedRectangle: {
+    backgroundColor: "#ECEFE8",
+    height: 187,
+    width: 330,
+    borderRadius: 32,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  emailInput: {
+    marginTop: 15,
+    fontFamily: "GartSerif",
+    borderColor: "#C8C2D3",
+    borderWidth: 4,
+    borderRadius: 29,
+    padding: 10,
+    width: 267,
+    height: 46,
+  },
+  signUpButton: {
+    elevation: 8,
+    backgroundColor: "#C8C2D3",
+    borderRadius: 32,
+    paddingVertical: 11,
+    paddingHorizontal: 70,
+    marginTop: 30,
+  },
+  buttonText: {
+    fontFamily: "GartSerif",
+    fontSize: 24,
+    color: "#2D2429",
+  },
+  createAccount: {
+    fontFamily: "GartSerif",
+    fontSize: 16,
+    color: "#ECEFE8",
+    marginTop: 15,
+  },
+});
